8366759f610dcd4728c1a29d0928c6e4eea553a1
E192479R <corentin.leandre@etu.univ-nantes.fr>
2021-03-26 14:32:02 +0100

    Added LettreMot type, with lettres reprensenting the number of each letters present in each word.

--- js/database/Mot.js
+++ js/database/Mot.js
@@ -1,58 +1,61 @@
 class BaseMot {
-    constructor(word){
-        Object.assign(this, word);
+    constructor(mot){
+        Object.assign(this, mot);
     }
 }
 
 class LettreMot {
-    constructor(word){
-        this.lettreA = 0;
-        this.lettreB = 0;
-        this.lettreC = 0;
-        this.lettreD = 0;
-        this.lettreE = 0;
-        this.lettreF = 0;
-        this.lettreG = 0;
-        this.lettreH = 0;
-        this.lettreI = 0;
-        this.lettreJ = 0;
-        this.lettreK = 0;
-        this.lettreL = 0;
-        this.lettreM = 0;
-        this.lettreN = 0;
-        this.lettreO = 0;
-        this.lettreP = 0;
-        this.lettreQ = 0;
-        this.lettreR = 0;
-        this.lettreS = 0;
-        this.lettreT = 0;
-        this.lettreU = 0;
-        this.lettreV = 0;
-        this.lettreW = 0;
-        this.lettreX = 0;
-        this.lettreY = 0;
-        this.lettreZ = 0;
-        this.lettreAgrave = 0;
-        this.lettreAcirconflexe = 0;
-        this.lettreAligature = 0;
-        this.lettreCcedille = 0;
-        this.lettreEaigu = 0;
-        this.lettreEgrave = 0;
-        this.lettreEcirconflexe = 0;
-        this.lettreEtrema = 0;
-        this.lettreIcirconflexe = 0;
-        this.lettreItrema = 0;
-        this.lettreOcirconflexe = 0;
-        this.lettreOligature = 0;
-        this.lettreUgrave = 0;
-        this.lettreUcirconflexe = 0;
-        this.lettreUtrema = 0;
-        this.lettreYtrema = 0;
 
-        for(const i = 0; i < word.length; i++){
+    static customCharValues = {
+        "a" : 0,
+        "b" : 1,
+        "c" : 2,
+        "d" : 3,
+        "e" : 4,
+        "f" : 5,
+        "g" : 6,
+        "h" : 7,
+        "i" : 8,
+        "j" : 9,
+        "k" : 10,
+        "l" : 11,
+        "m" : 12,
+        "n" : 13,
+        "o" : 14,
+        "p" : 15,
+        "q" : 16,
+        "r" : 17,
+        "s" : 18,
+        "t" : 19,
+        "u" : 20,
+        "v" : 21,
+        "w" : 22,
+        "x" : 23,
+        "y" : 24,
+        "z" : 25,
+        "à" : 26,
+        "â" : 27,
+        "æ" : 28,
+        "ç" : 29,
+        "è" : 30,
+        "é" : 31,
+        "ê" : 32,
+        "ë" : 33,
+        "î" : 34,
+        "ï" : 35,
+        "ô" : 36,
+        "œ" : 37,
+        "ù" : 38,
+        "û" : 39,
+        "ü" : 40,
+        "ÿ" : 41
+    }
 
-        }
+    constructor(mot){
+        this.mot = mot;
+        this.lettres = Array.apply(null, Array(42)).map(e => 0);
+        mot.split('').forEach(char => this.letters[LettreMot.customCharValues[char]] += 1);
     }
 }
 
-module.exports = {BaseMot};
+module.exports = {BaseMot, LettreMot};
